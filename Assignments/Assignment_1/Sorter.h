e54da813a3c864241505b627e265f7be7f69c77f
USMC1941 <billie888chen@outlook.com>
2017-09-19 11:01:20 -0400

    Add Assignment 1

---
+++ Assignments/Assignment_1/Sorter.h
@@ -0,0 +1,13 @@
+/*****
+*
+*	Define structures and function prototypes for your sorter
+*
+*
+*
+******/
+
+//Suggestion: define a struct that mirrors a record (row) of the data set
+
+
+//Suggestion: prototype a mergesort function
+
